75fdda22d3e8c5e8cde9b6ff340350ae2541845e
seppe <r0787714@thomasmore.be>
2020-10-18 17:38:35 +0200

    alles in orde

--- src/main/java/be/thomasmore/party/HomeController.java
+++ src/main/java/be/thomasmore/party/HomeController.java
@@ -2,14 +2,20 @@ package be.thomasmore.party;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
 
+    @Value("${value.from.file}")
+    private String valueFromFile;
+
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("valueFromFile",valueFromFile);
         return "test";
     }
 
